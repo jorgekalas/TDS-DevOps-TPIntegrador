42895473bdf671f267ed7b653bb1f259277cbbd1
jorgekalas <jorgekalas@gmail.com>
2025-11-14 12:37:00 -0300

    terraform fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,15 +1,42 @@
-provider "docker" {}
-
-resource "docker_image" "app" {
-  name         = "tds-devops-tpintegrador-app"
-  keep_locally = false
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
 }
 
+provider "docker" {}
+
+# Red interna para los contenedores
+resource "docker_network" "alquilarte_network" {
+  name = "alquilarte_network"
+}
+
+# Contenedor de MongoDB
+resource "docker_container" "mongo" {
+  name  = "alquilarte-mongo"
+  image = "mongo:6"
+  ports {
+    internal = 27017
+    external = 27017
+  }
+  networks_advanced {
+    name = docker_network.alquilarte_network.name
+  }
+}
+
+# Contenedor de la aplicación
 resource "docker_container" "app" {
   name  = "alquilarte-app"
-  image = docker_image.app.latest
+  image = "tds-devops-app:latest"
   ports {
     internal = 3000
     external = 3000
+  }
+  depends_on = [docker_container.mongo]
+  networks_advanced {
+    name = docker_network.alquilarte_network.name
   }
 }
